ab8e0e51d1f4abbe406685c2e894d2d86c7b9316
MrKira <50806393+MrKira128@users.noreply.github.com>
2019-10-26 20:18:34 +0200

    Create calculate-the-average-of-three-numbers.cpp

---
+++ cpp-projects/calculate-the-average-of-three-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+		double n1, n2, n3;
+    cout << "enter the first number: ";
+		cin >> n1;
+		cout << "enter the second number: ";
+		cin >> n2;
+		cout << "enter the third number: ";
+		cin >> n3;
+		cout << "the average is: " << (n1 + n2 + n1) / 3 <<endl;
+    return 0;
+}
