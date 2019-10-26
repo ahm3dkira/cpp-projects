2388ee7f93a8596208f16aeac67b0190eef9cd23
MrKira <50806393+MrKira128@users.noreply.github.com>
2019-10-26 18:38:28 +0200

    Create Circle-area.cpp

---
+++ cpp-projects/Circle-area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	double redias;
+	cout << "enter the redias number: ";
+	cin >> redias;
+	cout << "the * is: " << (redias *redias * 3.14) <<endl;
+
+}
