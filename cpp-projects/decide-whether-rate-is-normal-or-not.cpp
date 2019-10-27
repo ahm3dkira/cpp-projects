760e0729c0d26da9afd4da189050a1295a3ad5be
MrKira <50806393+MrKira128@users.noreply.github.com>
2019-10-27 21:49:39 +0200

    Create decide-whether-rate-is-normal-or-not.cpp

---
+++ cpp-projects/decide-whether-rate-is-normal-or-not.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    
+    cout << "enter hart rate: ";
+	cin >> r;
+	if (r == 70){
+	cout << "you are fine" << endl;
+	} else
+    cout << "you are not fine" << endl;
+    
+    return 0;
+}
