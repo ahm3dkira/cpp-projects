25ee42915ee560480079fd6145704e0910737ab2
MrKira <50806393+MrKira128@users.noreply.github.com>
2019-12-05 11:37:05 +0200

    using the switch case

---
+++ cpp-projects/program-to-check-whether-a-character-is-a-VOWEL-or-CONSONANT.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char ch ;
+    cout << "Enter char: ";
+    cin >>  ch ;
+	
+    switch(ch)
+    {
+        case 'a':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        case 'e':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        case 'i':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        case 'o':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        case 'u':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+	case 'y':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        case 'A':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        case 'E':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        case 'I':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        case 'O':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        case 'U':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+	case 'Y':
+            cout << " You Enter a Vowel character"<<endl;
+            break;
+        default:
+            cout << "You Enter a Consonant character"<<endl;
+    }
+    return 0;
+}
