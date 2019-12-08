bcdcc65a3683beaceec026e18d3ac6bbe5a42c6a
MrKira <50806393+MrKira128@users.noreply.github.com>
2019-12-08 23:14:28 +0200

    Update Fibonacci-Series.cpp

--- cpp-projects/Fibonacci-Series.cpp
+++ cpp-projects/Fibonacci-Series.cpp
@@ -4,7 +4,7 @@ int main() {
   int n1=0,n2=1,n3,i,number;    
  cout<<"Enter the number of elements: ";    
  cin>>number;    
- cout<<n1<<" "<<n2<<" "; //printing 0 and 1    
+ cout<<n1<<"\n"<<n2<<"\n"; //printing 0 and 1    
  for(i=2;i<number;++i) //loop starts from 2 because 0 and 1 are already printed    
  {    
   n3=n1+n2;    
