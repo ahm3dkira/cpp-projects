882871d883f3faca866931ff1c20f22429f5d780
MrKira <50806393+MrKira128@users.noreply.github.com>
2019-12-10 00:23:31 +0200

    Update prime-number-counter.cpp

--- cpp-projects/prime-number-counter.cpp
+++ cpp-projects/prime-number-counter.cpp
@@ -1,21 +1,20 @@
+/**************************************************************************
+ * made by MrKira :)                                                      *
+ * https://github.com/MrKira128/cpp-projects                              *
+ *https://www.youtube.com/c/MrKira128                                     *
+ **************************************************************************/
+
 //A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 
 #include <iostream>
-#include <math.h> //sqrt function used from this library
-
+#include <math.h> 
 using namespace std;
 
 int main(){
-// Variable Declaration 
 int n;
-
-// Get Input Value
  cout<<"Enter the Number : ";
  cin>>n;
-
 cout <<"List Of Prime Numbers Below "<<n<<endl;
-
-//for Loop Block For Find Prime Number 
 
 for (int i=2; i<n; i++)
  for (int j=2; j*j<=i; j++)
